c1b53f0c985db70020e9d77d9d134f3a9e0962d1
Hailey Pate <2haileypate@gmail.com>
2016-03-03 14:35:59 -0600

    start ETL with main script and utils
    this code yields a pandas dataframe that contains all original source
    data plus four new derived columns. these columns help answer the
    following questions about each source record:
    - Is this a cat or dog (yes/no)?
    - How many weeks old is the animal?
    - Is the animal a neonate, kitten/puppy, or adult?
    - Is this a live outcome?

---
+++ main.py
@@ -0,0 +1,44 @@
+from outcomeUtils import DeriveColumnLabels
+from outcomeUtils import DeriveNewValues as der
+
+import urllib, json
+import numpy as np
+import pandas as pd
+
+
+def main():
+
+	"""
+	import data
+
+	things to do: 
+	- add check for metadata to ensure no unexpected columns
+	"""
+
+	# request_url = "https://data.austintexas.gov/views/9t4d-g238/rows.json"
+	# response = urllib.request.urlopen(request_url)
+	# str_response = response.readall().decode('utf-8')
+
+	infile = 'test_data.json'
+	obj = json.loads(open(infile).read())
+
+	"""
+	prepare pandas dataframe
+	"""
+
+	data = pd.DataFrame(obj['data'])
+	data.columns = DeriveColumnLabels.get_labels(obj['meta']['view']['columns'])
+	
+
+	"""
+	append columns
+	"""
+
+	data['d_is_live_outcome'] = data.apply (lambda data: der.identify_live_outcomes(data.outcome_type),axis=1)
+	data['d_is_cat_or_dog'] = data.apply (lambda data: der.identify_cats_and_dogs(data.animal_type),axis=1)
+	data['d_age_in_weeks'] = data.apply (lambda data: der.calc_animal_age_in_weeks(data.datetime, data.date_of_birth),axis=1)
+	data['d_age_group'] = data.apply (lambda data: der.determine_age_group(data.d_age_in_weeks, data.d_is_cat_or_dog),axis=1)
+
+
+if __name__ == '__main__':
+	main()
---
+++ outcomeUtils.py
@@ -0,0 +1,58 @@
+from datetime import datetime, date
+import numpy as np
+import pandas as pd
+
+class DeriveColumnLabels(object):
+	"""
+	tbd
+	"""
+	def get_labels(columns):
+		labels = []
+		for i in columns:
+			fieldName = i['fieldName']
+			labels.append(fieldName)
+		return labels
+
+class DeriveNewValues(object):
+	"""
+	This class contains functions that use the original source data to derive new 
+	data values. Later, the AppendNewColumns class will place these values 
+	into new columns. 
+	"""
+	def identify_live_outcomes(outcome_type):
+		live_outcome_value_list = ['Adoption', 'Transfer', 'Return to Owner', 'Relocate']
+		if outcome_type in live_outcome_value_list:
+			is_live_outcome = 1
+		else:
+			is_live_outcome = 0
+		return is_live_outcome
+
+	def calc_animal_age_in_weeks(date_of_outcome, date_of_birth):
+		birth_date = date(int(date_of_birth[0:4]),int(date_of_birth[5:7]),int(date_of_birth[8:10]))
+		outcome_date = date(int(date_of_outcome[0:4]),int(date_of_outcome[5:7]),int(date_of_outcome[8:10]))
+		age = outcome_date - birth_date
+		age_in_days = age.days
+		age_in_weeks = age_in_days/7
+		return age_in_weeks
+
+	def identify_cats_and_dogs(type_of_animal):
+		dog_and_cat_value_list = ['Dog', 'Cat']
+		if type_of_animal in dog_and_cat_value_list:
+			is_cat_or_dog = 1
+		else:
+			is_cat_or_dog = 0
+		return is_cat_or_dog
+        
+	def determine_age_group(age_in_weeks, animal_type_test):
+		if animal_type_test ==0:
+			d_age_group = 'not cat or dog'
+		else:
+			if 0 < age_in_weeks < 6:
+				d_age_group = 'neonate'
+			elif 6 <= age_in_weeks < 52:
+				d_age_group = 'kitten/puppy'
+			elif 52 <= age_in_weeks:
+				d_age_group = 'adult'
+			else:
+				d_age_group = 'unable_to_determine'
+		return d_age_group
